8672e176e3e2b4b74e46ebf772f8d6c0e19a3891
侯亚迪 <houyadi@houyadideMacBook-Pro.local>
2018-05-08 16:01:10 +0800

    代码整理

--- LocalHTTPServer/AppDelegate.h
+++ LocalHTTPServer/AppDelegate.h
@@ -3,6 +3,7 @@
 //  LocalHTTPServer
 //
 //  Created by 侯亚迪 on 2018/1/28.
+//  Copyright © 2017年 侯亚迪. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- LocalHTTPServer/YDHTTPConnection.h
+++ LocalHTTPServer/YDHTTPConnection.h
@@ -3,7 +3,7 @@
 //  Kitty
 //
 //  Created by 侯亚迪 on 2017/10/30.
-//  Copyright © 2017年 WeiCai. All rights reserved.
+//  Copyright © 2017年 侯亚迪. All rights reserved.
 //
 
 #import "HTTPConnection.h"
